7461389b0f3ae8034a9e5358482fc4cd8d964843
ketai <edward@arducam.com>
2022-09-15 16:25:17 +0800

    A usable preliminary version.

--- Detect_HAT_Firmware_Date.py
+++ Detect_HAT_Firmware_Date.py
@@ -4,35 +4,42 @@
 # Copyright (C) 2022, Arducam.
 #
 
+import subprocess
+import argparse
         
- class Report:
+class Report:
     def __init__(self, csiport):
         self.csiport = csiport
-    def run_cmd(cmd):
+    def run_cmd(self, cmd):
         try:
             p = subprocess.run(cmd, universal_newlines=True, check=False, shell=True,
                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            return p
+            return p.stdout
         except RuntimeError as e:
             print(f'Error: {e}')
             
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='fireware date generator')
-    parser.add_argument('-y', help='csi port', type=int)
+    parser.add_argument('-y', help='csi port', type=int, required=True)
     args = parser.parse_args()
     
     i2caddress_Hat = 0x24
     i2caddress_year = 0x05
     i2caddress_month = 0x06
-    i2caddress_date = 0x05
+    i2caddress_date = 0x07
     
     csi = Report(args.y)
-    year = csi.run_cmd("i2cget -y {} {} {}".format(args.o, i2caddress_Hat, i2caddress_year))
-    month = csi.run_cmd("i2cget -y {} {} {}".format(args.o, i2caddress_Hat, i2caddress_month))
-    date = csi.run_cmd("i2cget -y {} {} {}".format(args.o, i2caddress_Hat, i2caddress_date))
-    print(year)
-    print(month)
-    print(date)
+    year = csi.run_cmd("i2cget -y {} {} {}".format(args.y, i2caddress_Hat, i2caddress_year)).strip()
+    month = csi.run_cmd("i2cget -y {} {} {}".format(args.y, i2caddress_Hat, i2caddress_month)).strip()
+    date = csi.run_cmd("i2cget -y {} {} {}".format(args.y, i2caddress_Hat, i2caddress_date)).strip()
+
+    year = "20" + str(int(year, 16))
+    month = str(int(month, 16))
+    date = str(int(date, 16))
+
+
+    time = year + "-" + month + "-" + date
+    print(time)
     
     
