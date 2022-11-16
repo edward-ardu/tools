f5e944e7e6c376cd63cadbe178e48632ec4d9b94
ketai <edward@arducam.com>
2022-11-16 09:44:32 +0800

    Optimization register_debug_tool

--- register_debug_tool.sh
+++ register_debug_tool.sh
@@ -1,5 +1,9 @@
 # /bin/bash
 
+BIT=`getconf LONG_BIT`
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 function rand(){
     min=$1
     max=$(($2-$min+1))
@@ -7,8 +11,45 @@ function rand(){
     echo $(($num%$max+$min))
 }
 
+installFile() {
+    
+    CAMERA_I2C_FILE_NAME="camera_i2c"
+    CAMERA_I2C_FILE_DOWNLOAD_LINK="https://raw.githubusercontent.com/ArduCAM/MIPI_Camera/master/RPI/utils/camera_i2c"
+    RPI3_GPIOVIRTBUF_FILE_NAME="rpi3-gpiovirtbuf"
 
-for((i=1;i<=100000;i++));  
+    if [ ! -s $CAMERA_I2C_FILE_NAME ]; then
+        wget -O $CAMERA_I2C_FILE_NAME $CAMERA_I2C_FILE_DOWNLOAD_LINK
+        chmod +x $CAMERA_I2C_FILE_NAME
+    fi
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Download failed.${NC}"
+        echo "Please check your network and try again."
+        exit -1
+    fi
+
+    if [ "$BIT" = 32 ]; then
+        RPI3_GPIOVIRTBUF_FILE_DOWNLOAD_LINK="https://github.com/ArduCAM/MIPI_Camera/raw/master/RPI/utils/rpi3-gpiovirtbuf/32/rpi3-gpiovirtbuf"
+    elif [ "$BIT" = 64 ]; then
+        RPI3_GPIOVIRTBUF_FILE_DOWNLOAD_LINK="https://github.com/ArduCAM/MIPI_Camera/raw/master/RPI/utils/rpi3-gpiovirtbuf/64/rpi3-gpiovirtbuf"
+    fi
+
+    if [ ! -s $RPI3_GPIOVIRTBUF_FILE_NAME ]; then
+        wget -O $RPI3_GPIOVIRTBUF_FILE_NAME $RPI3_GPIOVIRTBUF_FILE_DOWNLOAD_LINK
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Download failed.${NC}"
+        echo "Please check your network and try again."
+        exit -1
+    else
+        chmod +x $RPI3_GPIOVIRTBUF_FILE_NAME
+        sudo ./camera_i2c>/dev/null 2>&1
+    fi
+}
+
+installFile
+
+for((i=1;i<=1000;i++));  
 do 
 
 rnd=$(rand 1 99)
